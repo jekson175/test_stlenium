27dca2a8ebaa063095f2a0e6917d4ec4a7bf2b4d
jekson175 <jekson175@gmail.com>
2023-07-21 22:31:08 +0700

    first2

--- TEST_NAME1.py
+++ TEST_NAME1.py
@@ -10,7 +10,7 @@ class RecorderTest(BaseCase):
         self.click("body main div div div div:nth-of-type(3) a")
         self.open_if_not_url("https://accounts.google.com/signup/v2/createaccount?service=mail&continue=https://mail.google.com/mail/&flowName=GlifWebSignIn&flowEntry=SignUp")
         self.type("input#firstName", "fgdfgdgsf")
-        self.type("input#lastName", "fdgsdfgfdg")
+        self.type("input#lastName", "1111111111111111111")
         self.click("div.VfPpkd-RLmnJb")
         self.type("input#day", "12")
         self.select_option_by_text("select#month", "Январь")
